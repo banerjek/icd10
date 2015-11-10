3f587de77496777519c1fb8386a226807a48d570
Kyle Banerjee <banerjek@ohsu.edu>
2015-11-10 14:52:56 -0800

    improve tree searching and add tips

--- processmesh.sh
+++ processmesh.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-cat mesh.txt |grep "^MH \|^MN" |sed 's/MN = \(.*$\)/<c>\1<\/c>/' |tr -d "\n" |sed 's/MH = /@/g' > mesh.js
+cat mesh.txt |grep "^MH \|^MN" |sed 's/MN = \(.*$\)/<c>[\1]<\/c>/' |tr -d "\n" |sed 's/MH = /@/g' > mesh.js
